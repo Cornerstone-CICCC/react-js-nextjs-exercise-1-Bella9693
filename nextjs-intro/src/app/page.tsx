78ea8517d55c20417655e2bd65e44c162bdd902f
Bella9693 <realbella9693@gmail.com>
2026-02-03 15:08:31 -0800

    exercise completed

---
+++ nextjs-intro/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Home</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </main>
+  );
+}
---
+++ nextjs-intro/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 text-center p-4 mt-auto">
+      <p>© 2026 NextJS Intro. All rights reserved.</p>
+    </footer>
+  );
+}
